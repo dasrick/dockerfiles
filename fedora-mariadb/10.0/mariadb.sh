f8753a1fe7cbaeadee8661cc33f7a7240c4fff55
Enrico Hoffmann <enrico.hoffmann@movingimage24.com>
2014-12-31 11:56:24 +0100

    changes for dynamic password

--- fedora-mariadb/10.0/mariadb.sh
+++ fedora-mariadb/10.0/mariadb.sh
@@ -2,14 +2,43 @@
 chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --user mysql > /dev/null
 
-mysqld_safe --user mysql &
+#mysqld_safe --user mysql &
+#sleep 5s
 
-sleep 5s
+#mysql -v < /mariadb.sql
+#sleep 5s
 
-mysql -v < /mariadb.sql
+#ps -wef | grep mysql | grep -v grep | awk '{print $2}' | xargs kill -9
 
-sleep 5s
+#mysqld_safe --user mysql
 
-ps -wef | grep mysql | grep -v grep | awk '{print $2}' | xargs kill -9
+#####
 
-mysqld_safe --user mysql
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MariaDB service startup"
+    sleep 5
+    mysql -uroot -e "status" > /dev/null 2>&1
+    RET=$?
+done
+
+
+PASS=${MARIADB_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${MARIADB_PASS} ] && echo "preset" || echo "random" )
+echo "=> Creating MariaDB admin user with ${_word} password"
+
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+
+echo "=> Done!"
+
+echo "========================================================================"
+echo "You can now connect to this MariaDB Server using:"
+echo ""
+echo "    mysql -uadmin -p$PASS -h<host> -P<port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "MariaDB user 'root' has no password but only allows local connections"
+echo "========================================================================"
+
+mysqladmin -uroot shutdown
